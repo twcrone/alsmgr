01ce6c4fd4ecbe2703022b954c3912d15194cb47
Todd W Crone <twcrone@gmail.com>
2022-01-27 09:58:11 -0500

    Start putting into proper functions

--- alsmgr.cr
+++ alsmgr.cr
@@ -1,13 +1,25 @@
-alias_file_location = Path["~/.alias"].expand(home: true)
-hash = Hash(String, String).new
-File.each_line(alias_file_location) do |line|
-  values = line.split("=")
-  first = values[0]
-  name = first.split(" ")[1]
-  path = values[1]
-  hash[name] = path
+def load_aliases_from(alias_file_location) : Hash(String, String)
+  hash = Hash(String, String).new
+  File.each_line(alias_file_location) do |line|
+    values = line.split("=")
+    first = values[0]
+    name = first.split(" ")[1]
+    path = values[1]
+    hash[name] = path
+  end
+  hash
 end
 
+def get_output_for(aliases : Hash(String, String))
+  a = Array(String).new
+  aliases.each do |name, path|
+    a << name + "=" + path
+  end
+  output = a.sort!.join("\n")
+end
+
+alias_file_location = Path["~/.alias"].expand(home: true)
+hash = load_aliases_from(alias_file_location)
 new_name = ARGV[0]?
 
 if new_name
@@ -32,4 +44,4 @@ aliases.each do |s|
   new_file_content += "alias #{s}\n"
 end
 
-puts new_file_content
+print_aliases hash
