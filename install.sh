d2d0952613af5a51e233f98d5ba0e01875b0040d
Todd Crone <twcrone@gmail.com>
2022-06-23 10:35:41 -0400

    Make bin directory in user home if does not exist

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 echo "Building alsmgr"
 crystal build alsmgr.cr --release
+echo "Creating a ~/bin directory if it does not already exist"
+mkdir ~/bin
 echo "Copying to ~/bin"
 cp alsmgr ~/bin/.
 cp alsmgr ~/bin/am
